684f045c5ae51145214ace1725886087bae67997
Bart M.N. Smets <bmnsmets@protonmail.com>
2025-07-23 12:49:42 +0200

    SE(2) lifting model

--- models/lifting_conv.py
+++ models/lifting_conv.py
@@ -4,6 +4,7 @@ from multi_conv import MultiConv2d
 from torch import Tensor
 import math
 import torch.nn.functional as F
+from typing import Literal
 
 
 def rotation_resampling_grid(
@@ -55,6 +56,7 @@ class SE2Lifting(nn.Module):
         device=None,
         dtype=None,
         zero_mean: bool = True,
+        sn_size: int = 256,
     ):
         super().__init__()
         assert len(num_channels) > 1
@@ -64,6 +66,7 @@ class SE2Lifting(nn.Module):
         self.num_channels = num_channels
         self.orientations = orientations
         self.zero_mean = zero_mean
+        self.sn_size = sn_size
         if isinstance(kernel_size, int):
             kernel_size = (kernel_size,) * (len(num_channels) - 1)
         assert len(kernel_size) == len(num_channels) - 1
@@ -74,14 +77,20 @@ class SE2Lifting(nn.Module):
 
         self.kernel_size = kernel_size
         self.eff_kernel_size = eff_kernel_size
+        self.padding_total = sum(size // 2 for size in kernel_size)
 
+        # grid locations for resampling rotated version of the mother kernels
         grid = rotation_resampling_grid(
             orientations, eff_kernel_size, align_corners=False
         )
-        self.register_buffer("grid", grid)
+        self.register_buffer("grid", grid.to(device))
 
+        # mask to make the effective kernels supported in a circle
         mask = rotation_mask(eff_kernel_size)
-        self.register_buffer("mask", mask)
+        self.register_buffer("mask", mask.to(device))
+
+        # caching the spectral norm estimate
+        self.L = torch.ones(1, requires_grad=True, device=device)
 
         self.weights = nn.ParameterList(
             [
@@ -93,7 +102,19 @@ class SE2Lifting(nn.Module):
             ]
         )
 
+        dsize = eff_kernel_size + 2 * (eff_kernel_size // 2)
+        self.dirac = torch.zeros((1, 1, dsize, dsize), **factory_kwargs)
+        self.dirac[0, 0, eff_kernel_size, eff_kernel_size] = 1
+
         self.reset_parameters()
+
+    @property
+    def device(self):
+        return self.weights[0].device
+
+    @property
+    def dtype(self):
+        return self.weights[0].device
 
     def reset_parameters(self) -> None:
         for w in self.weights:
@@ -104,6 +125,11 @@ class SE2Lifting(nn.Module):
     def sample_kernels(self) -> Tensor:
         assert isinstance(self.mask, Tensor)
         assert isinstance(self.grid, Tensor)
+        ws = self.weights
+        if self.zero_mean:
+            ws[-1] = ws[-1] - torch.mean(ws[-1], dim=(1, 2, 3)).view(
+                ws[-1].size(0), 1, 1, 1
+            )
         w = fuse_kernels_2d(self.weights) * self.mask
         ksize = w.shape[-2:]
         w = w.view(1, -1, *ksize)  # [1,Cn*C0,H,W]
@@ -112,12 +138,61 @@ class SE2Lifting(nn.Module):
         w = w.view(
             self.orientations, self.num_channels[-1], self.num_channels[0], *ksize
         )
-        if self.zero_mean:
-            w -= torch.mean(w * self.mask, dim=(2, 3, 4)).view(*w.shape[:2], 1, 1, 1)
+        # if self.zero_mean:
+        #     w -= torch.mean(w * self.mask, dim=(2, 3, 4)).view(*w.shape[:2], 1, 1, 1)
         return w * self.mask  # [Nor, Cout, Cin, kH, kW]
 
     def forward(self, x: Tensor):
-        w = self.sample_kernels()
+        return self.convolution(x)
+
+    def convolution(self, x: Tensor):
+        x = x / torch.sqrt(self.L)  # [B,Cin,H,W]
+        w = self.sample_kernels()  # [NOr,Cout,Cin,kH,kH]
+        w = w.view(-1, *w.shape[-3:])  # [NOr*Cout,Cin,kH,kH]
+        y = F.conv2d(
+            x, w, bias=None, padding=(self.eff_kernel_size // 2)
+        )  # [B,NOr*Cout,H,W]
+        y = y.reshape(x.size(0), self.orientations, -1, *y.shape[-2:])
+        return y  # [B,NOr,Cout,H,W]
+
+    def transpose(self, x: Tensor):
+        x = x / torch.sqrt(self.L)  # [B,Nor,Cout,H,W]
+        x = x.reshape(x.size(0), -1, x.size(-2), x.size(-1))  # [B,Nor*Cout,H,W]
+        w = self.sample_kernels()  # [NOr,Cout,Cin,kH,kH]
+        w = w.view(-1, *w.shape[-3:])  # [NOr*Cout,Cin,kH,kH]
+        y = F.conv_transpose2d(x, w, bias=None, padding=(self.eff_kernel_size // 2))
+        return y  # [B,Cin,H,W]
+
+    def get_kernel_WtW(self):
+        return self.transpose(self.convolution(self.dirac.to(self.device)))
+
+    def spectrum(self):
+        kernel = self.get_kernel_WtW()
+        padding = (self.sn_size - 1) // 2 - self.padding_total
+        return torch.fft.fft2(F.pad(kernel, (padding,) * 4))
+
+    def spectral_norm(
+        self,
+        mode: Literal["Fourier"] | Literal["power_method"] = "Fourier",
+        n_steps: int = 1000,
+    ) -> Tensor:
+        if mode == "Fourier":
+            self.L = self.spectrum().abs().max()
+        elif mode == "power_method":
+            u = torch.randn((1, 1, self.sn_size, self.sn_size), device=self.device)
+            with torch.no_grad():
+                for _ in range(n_steps):
+                    u = self.transpose(self.convolution(u))
+                    u = u / torch.linalg.norm(u)
+            self.L = torch.linalg.norm(self.transpose(self.convolution(u)))
+
+        return self.L
+
+    def __repr__(self):
+        s1 = f"num_channels={self.num_channels}"
+        s2 = f"kernel_size={self.kernel_size}"
+        s3 = f"orientations={self.orientations}"
+        return f"{self.__class__.__name__}({s1}, {s2}, {s3})"
 
 
 if __name__ == "__main__":
@@ -125,11 +200,11 @@ if __name__ == "__main__":
 
     m = SE2Lifting(num_channels=(1, 2, 4, 8), kernel_size=(5, 5, 5), orientations=16)
 
-    with torch.no_grad():
-        m.weights[0][0, 0] = 0
-        m.weights[0][0, 0, 2, 2:] = 1
-        m.weights[1][0, 0] = 0
-        m.weights[1][0, 0, 1:3, 1:3] = 1
+    # with torch.no_grad():
+    #     m.weights[0][0, 0] = 0
+    #     m.weights[0][0, 0, 2, 2:] = 1
+    #     m.weights[1][0, 0] = 0
+    #     m.weights[1][0, 0, 1:3, 1:3] = 1
     k = m.sample_kernels()
     for i in range(m.orientations // 2 + 1):
         f = k[i, 0, 0].detach()
